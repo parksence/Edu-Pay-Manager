5848adead22ff2518b889f0e5ef138266d846f8c
hjpark <parksence95@gmail.com>
2026-02-24 15:27:41 +0900

    github page로 배포할 수 있도록 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/Edu-Pay-Manager/', // GitHub Pages 프로젝트 경로
   plugins: [react()],
   resolve: {
     alias: {
